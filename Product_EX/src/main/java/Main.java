9427f8b131a66376023b8937c9bb81a3c3582eff
Garam Son <forelise25@naver.com>
2018-05-01 20:43:53 +0900

    #second 스프링 xml방식

--- Product_EX/src/main/java/Main.java
+++ Product_EX/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 import exam.product.dao.ProductDao;
 import exam.product.dao.ProductImpl;
 import exam.product.service.ProductService;
@@ -6,10 +9,15 @@ import exam.product.ui.ProductUI;
 
 public class Main {
 	public static void main(String[] args){
+		ApplicationContext ctx = new GenericXmlApplicationContext("config.xml");
+		ProductUI productUI = (ProductUI)ctx.getBean("productUI");
+		productUI.showMenu();
+		/*
 		ProductDao productDao = new ProductImpl();
 		ProductService productService = new ProductServiceImpl(productDao);
 		ProductUI productUI = new ProductUI();
 		productUI.setProductService(productService);
 		productUI.showMenu();	
+		*/
 	}
 }
